2fb07e6fdf2551f0c2371a350bdc63c09c364a06
anandamuniz <muniz.ananda@gmail.com>
2022-08-26 16:31:40 -0300

    primeiro commit

---
+++ js/firebase/hellfire-clube.js
@@ -0,0 +1,10 @@
+import app from './firebase/app.js'
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
+
+
+export async function subscribeToHellfireClub(subScription) {
+    const db = getFirestore(app)
+    const hellfireClubCollection = collection(db, 'hellfire-clube')
+    const docRef = await addDoc(hellfireClubCollection, subScription)
+    return docRef.id
+}
---
+++ js/main.js
@@ -0,0 +1,20 @@
+import { subscribeToHellfireClub } from './firebase/hellfire-clube.js'
+
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+btnSubscribe.addEventListener('click', async () => {
+    const subScription = {
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
+        character: txtCharacter.value
+    }
+   
+    const subscriptionId = await subscribeToHellfireClub(subScription)
+    console.log(`Inscrito com sucesso: ${subscriptionId} .`)
+})
+
